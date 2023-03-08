dcc8bf2c85726a4d6a9d22e110f0db0f03e28ece
Miles Holder <mwholder2005@gmail.com>
2023-03-08 16:25:22 -0600

    Apotheosis Balences

--- auto/kubejs_export.py
+++ auto/kubejs_export.py
@@ -22,7 +22,8 @@ def copy_and_overwrite(from_path, to_path):
     if os.path.exists(to_path):
         shutil.rmtree(to_path)
     shutil.copytree(from_path, to_path)
-    
+
+
 def verify_and_clean(from_path, to_path):
     """Verifies the FROM_PATH and TO_PATH are the same, and warns if they are not"""
     if not os.path.exists(from_path):
@@ -36,13 +37,14 @@ def verify_and_clean(from_path, to_path):
             from_file = os.path.join(root, file)
             to_file = os.path.join(to_path, from_file[len(from_path)+1:])
             if not os.path.exists(to_file):
-                print(f"ERROR: TO_FILE {to_file} does not exist! (FROM_FILE: {from_file})")
+                print(
+                    f"ERROR: TO_FILE {to_file} does not exist! (FROM_FILE: {from_file})")
                 continue
             if not filecmp.cmp(from_file, to_file):
-                print(f"ERROR: TO_FILE {to_file} is not the same as FROM_FILE {from_file}!")
+                print(
+                    f"ERROR: TO_FILE {to_file} is not the same as FROM_FILE {from_file}!")
                 continue
             print(f"Verified: {to_file}")
-            
 
 
 if __name__ == "__main__":
@@ -52,3 +54,4 @@ if __name__ == "__main__":
     print(f"KubeJS path: {kubejs_path}")
     copy_and_overwrite("kubejs", kubejs_path)
     verify_and_clean(kubejs_path, "kubejs")
+    print("Done! :D /reload in-game to load the new scripts!")
